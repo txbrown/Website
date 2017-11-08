e3c723d1df630e1baf51b999dbacbaddff1ac84a
Ricardo Abreu <rj.abreu89@gmail.com>
2017-11-08 01:39:52 +0000

    cleanup

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,10 +4,11 @@ import styled from "styled-components";
 import Container from "../components/Container";
 import JumboTitle from "../components/JumboTitle";
 import Title from "../components/Title";
-import photoCanvasImage from "../assets/images/project-1.png";
-import cardsMenuConcept from "../assets/images/project-2.png";
 import { Section, Image } from "../components/base/StyledComponents";
 import { Flex, Box } from "grid-styled";
+import ProjectCard from "../components/ProjectCard";
+import photoCanvasImage from "../assets/images/project-1.png";
+import cardsMenuConcept from "../assets/images/project-2.png";
 
 const ProjectList = styled.ul`
   display: flex;
@@ -20,26 +21,6 @@ const Item = styled.li`
   width: 50%;
 `;
 
-const ProjectCard = styled.div`
-  width: 100%;
-  position: relative;
-  display: block;
-`;
-
-const ProjectCardCover = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  color: #fff;
-  background: ${props => props.background};
-  opacity: 0;
-  transition: opacity 0.5s ease-out,
-    transform 0.5s cubic-bezier(0.215, 0.61, 0.355, 1),
-    -webkit-transform 0.5s cubic-bezier(0.215, 0.61, 0.355, 1);
-`;
-
 const Projects = () => (
   <Container>
     <Section>
@@ -47,28 +28,18 @@ const Projects = () => (
 
       <ProjectList>
         <Item>
-          <ProjectCard>
-            <Image src={photoCanvasImage} alt="" />
-            <ProjectCardCover background="#ffeb5c">
-              <h3 className="project-card-title">PhotoCanvas - UWP</h3>
-            </ProjectCardCover>
-          </ProjectCard>
+          <ProjectCard
+            image={photoCanvasImage}
+            title="PhotoCanvas - UWP"
+            highlightcColor="#ffeb5c"
+          />
         </Item>
         <Item>
-          <ProjectCard>
-            <Image src={cardsMenuConcept} alt="" />
-            <ProjectCardCover background="#33D399">
-              <h3 className="project-card-title">Cards Menu Concept</h3>
-            </ProjectCardCover>
-          </ProjectCard>
-        </Item>
-        <Item>
-          <ProjectCard>
-            <Image src="/public/images/project-2.png" alt="" />
-            <ProjectCardCover background="#33D399">
-              <h3 className="project-card-title">Cards Menu Concept</h3>
-            </ProjectCardCover>
-          </ProjectCard>
+          <ProjectCard
+            image={cardsMenuConcept}
+            title="Cards Menu Concept"
+            highlightcColor="#33D399"
+          />
         </Item>
       </ProjectList>
       {/* <Flex justify="center">
